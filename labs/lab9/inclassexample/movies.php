8e023fb35cc72b31c5f044d43d9cff6958bd0ce5
ChrisJH07 <hernac7@rpi.edu>
2026-04-20 19:14:45 -0400

    Connecting php
    Fixed index.php to connect to my iit and completed movie.php

--- labs/lab9/inclassexample/movies.php
+++ labs/lab9/inclassexample/movies.php
@@ -1,17 +1,59 @@
 <?php 
-  include('includes/init.inc.php'); // include the DOCTYPE and opening tags
-  include('includes/functions.inc.php'); // functions
+include('includes/init.inc.php');
+include('includes/functions.inc.php');
 ?>
-<title>PHP &amp; MySQL - ITWS</title>   
+
+<title>Movies</title>
 
 <?php include('includes/head.inc.php'); ?>
 
-<h1>PHP &amp; MySQL</h1>
-      
+<h1>Movies</h1>
+
 <?php include('includes/menubody.inc.php'); ?>
 
-<p>Build the movie forms and output here.</p>
+<?php
+$dbOk = false;
 
-<?php include('includes/foot.inc.php'); 
-  // footer info and closing tags
+@$db = new mysqli('localhost', 'root', 'root', 'iit');
+
+if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+} else {
+    $dbOk = true;
+}
 ?>
+
+<h3>Movie List</h3>
+
+<table>
+<?php
+if ($dbOk) {
+
+    $query = 'SELECT * FROM movies ORDER BY title';
+    $result = $db->query($query);
+    $numRecords = $result->num_rows;
+
+    echo '<tr><th>Title</th><th>Year</th></tr>';
+
+    for ($i = 0; $i < $numRecords; $i++) {
+        $record = $result->fetch_assoc();
+
+        if ($i % 2 == 0) {
+            echo '<tr>';
+        } else {
+            echo '<tr class="odd">';
+        }
+
+        echo '<td>' . htmlspecialchars($record['title']) . '</td>';
+        echo '<td>' . htmlspecialchars($record['year']) . '</td>';
+        echo '</tr>';
+    }
+
+    $result->free();
+    $db->close();
+}
+?>
+</table>
+
+<?php include('includes/foot.inc.php'); ?>
